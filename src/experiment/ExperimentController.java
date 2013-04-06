bdfd15dbef4e2f08b5bd631890c6d73d0597c292
Connor Schenck <raenottingham@gmail.com>
2013-04-06 15:47:14 -0500

    Changed constants

--- src/experiment/ExperimentController.java
+++ src/experiment/ExperimentController.java
@@ -28,7 +28,7 @@ import utility.Utility;
 public class ExperimentController {
 	
 	private static final int NUM_SAMPLES = 200;
-	public static final int NUM_THREADS = 8;
+	public static final int NUM_THREADS = 16;
 	public final static int NUM_CHOICES = 10;
 	private static final String RESULTS_PATH = "results";
 	
