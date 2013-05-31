ded954b998a370bf5382c9951a97c78a83bbc63f
Connor Schenck <raenottingham@gmail.com>
2013-05-31 15:58:24 -0500

    Modified results processor and put features in a zip file

--- src/testingStuff/ResultsProcessor.java
+++ src/testingStuff/ResultsProcessor.java
@@ -75,8 +75,8 @@ public class ResultsProcessor {
 	 */
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 //		aggregateResults("results/results.txt");
-//		processResults("results/aggregateResults.txt");
-		computeTables("results/aggregateResults.txt");
+		processResults("results/aggregateResults.txt");
+//		computeTables("results/aggregateResults.txt");
 //		exampleTask();
 	}
 	
